f86ea411632e9b15d377b80f9bff405c48821554
RJ Nowling <rnowling@gmail.com>
2015-03-31 11:28:03 -0500

    Update nginx proxying in preparation for authentication

---
+++ nginx_docker/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ 
+# From https://gist.github.com/xaviervia/6adea3ddba269cadb794
+
+# Preprend the upstream configuration
+sed -i s/ZEPPELIN_SERVER/${ZEPPELIN_PORT_8080_TCP_ADDR}/g /etc/nginx/nginx.conf 
+
+# log result
+cat /etc/nginx/nginx.conf
+ 
+# Start nginx
+nginx
