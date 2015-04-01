4d93fb5e712e376e3134900e822fd63efb67f7ee
RJ Nowling <rnowling@gmail.com>
2015-04-01 12:05:05 -0500

    Add env output to nginx docker container

--- nginx_docker/run.sh
+++ nginx_docker/run.sh
@@ -2,6 +2,8 @@
  
 # From https://gist.github.com/xaviervia/6adea3ddba269cadb794
 
+env
+
 # Preprend the upstream configuration
 sed -i s/ZEPPELIN_SERVER/${ZEPPELIN_PORT_8080_TCP_ADDR}/g /etc/nginx/nginx.conf 
 
